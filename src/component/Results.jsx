7c32c6d68bc24317bf499d5f11c5304eb6cdfeb4
Nicklas Tiger <nicklas.tiger@gmail.com>
2025-01-28 08:20:27 +0100

    Last, i promise. Swapped to swedish in resultscreen

--- src/component/Results.jsx
+++ src/component/Results.jsx
@@ -5,10 +5,8 @@ const Results = ({ score, questions, onPlayAgain }) => {
     <div className="container text-center mt-4">
       <h3 className="text-success">Även livets goda tar slut ibland</h3>
       <p className="lead">
-        Djuuuuuuuuuuuuuuuuuuuuu!
-        <br />
-        Your score: <strong>{score}</strong> out of{" "}
-        <strong>{questions.length}</strong>
+        Du svarade rätt på <strong>{score}</strong> av{" "}
+        <strong>{questions.length}</strong> frågor!
       </p>
       <button className="btn btn-secondary" onClick={onPlayAgain}>
         Spela igen!
